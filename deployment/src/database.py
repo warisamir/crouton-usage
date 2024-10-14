a85b575a76d0d290811dcb2a3bd98b6b27ef25ff
Samir Atpadkar <samatpadkar183@gmail.com>
2024-10-14 14:27:51 +0530

    Integrate Pydantic V2 and patch CRUD routes

--- deployment/src/database.py
+++ deployment/src/database.py
@@ -6,20 +6,24 @@ import os
 
 load_dotenv()
 
-try:
-    SQL_DB_FILENAME = os.getenv("SQL_DB_FILENAME")
-    SQLALCHEMY_DATABASE_URL = "sqlite:///"+SQL_DB_FILENAME
-    
-except Exception as e:
-    SQLALCHEMY_DATABASE_URL = "sqlite:////app/db/app.db"
-    print('creating new db @', SQLALCHEMY_DATABASE_URL)
+SQL_DB_FILENAME = os.getenv("SQL_DB_FILENAME")
 
-
+if SQL_DB_FILENAME:
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{SQL_DB_FILENAME}"
+    print(f"Database will be located at: {os.path.abspath(SQL_DB_FILENAME)}")
+else:
+   
+    SQLALCHEMY_DATABASE_URL = "sqlite:////app/db/app.db"  
+    print('Creating new db @', SQLALCHEMY_DATABASE_URL)
+db_directory = os.path.dirname(SQL_DB_FILENAME) if SQL_DB_FILENAME else "/app/db/"
+os.makedirs(db_directory, exist_ok=True) 
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
 Base = declarative_base()
+
 event.listen(engine, 'connect', lambda c, _: c.execute('pragma foreign_keys=on'))
