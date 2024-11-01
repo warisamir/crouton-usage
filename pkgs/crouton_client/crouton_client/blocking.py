e77d038d102150e63a3f5fda97c9ca491d936960
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-11-01 03:22:54 -0500

    Update blocking.py

--- pkgs/crouton_client/crouton_client/blocking.py
+++ pkgs/crouton_client/crouton_client/blocking.py
@@ -6,62 +6,51 @@ class CroutonClient:
         self.API_ROOT = API_ROOT
         self.ACCESS_STRING = ACCESS_STRING
 
-    def api_get_call(resource: str, item_id: str = None):
+    def api_get_call(self, resource: str, item_id: str = None):
         if item_id:
-            res = r.get(API_ROOT+resource+'/'+item_id+ACCESS_STRING)
-            if res.status_code == 200:
-                return res.json()
-            else:
-                print(res.status_code)
-                print(res.json())
-                
-                # Test
-                raise ValueError(res.status_code)
+            res = r.get(self.API_ROOT + resource + '/' + item_id + self.ACCESS_STRING)
         else:
-            res = r.get(API_ROOT+resource+ACCESS_STRING)
-            if res.status_code == 200:
-                return res.json()
-            else:
-                print(res.status_code)
-                print(res.json())
-                
-                # Test
-                raise ValueError(res.json())
+            res = r.get(self.API_ROOT + resource + self.ACCESS_STRING)
+            
+        if res.status_code == 200:
+            return res.json()
+        else:
+            print(res.status_code)
+            print(res.json())
+            raise ValueError(res.status_code)
 
-
-    def api_post_call(resource: str, data_obj: dict):
+    def api_post_call(self, resource: str, data_obj: dict):
         if 'id' not in data_obj:
-                data_obj.update({'id': UUIDGenerator().create()})
-        res = r.post(API_ROOT+resource+ACCESS_STRING, 
-                json=data_obj)
+            data_obj.update({'id': UUIDGenerator().create()})
+            
+        res = r.post(self.API_ROOT + resource + self.ACCESS_STRING, json=data_obj)
+        
         if res.status_code == 200:
             return res.json()
         else:
             print(res.status_code)
             print(res.json())
-            raise ValueError
+            raise ValueError(res.status_code)
 
-
-    def api_put_call(resource: str, data_obj: dict, item_id: str):
-        res = r.put(API_ROOT+resource+'/'+item_id+ACCESS_STRING, 
-                json=data_obj)
+    def api_put_call(self, resource: str, data_obj: dict, item_id: str):
+        res = r.put(self.API_ROOT + resource + '/' + item_id + self.ACCESS_STRING, json=data_obj)
+        
         if res.status_code == 200:
             return res.json()
         else:
             print(res.status_code)
             print(res.json())
-            raise ValueError
+            raise ValueError(res.status_code)
 
-
-    def api_delete_call(resource: str, item_id: str = None):
+    def api_delete_call(self, resource: str, item_id: str = None):
         if item_id:
-            res = r.delete(API_ROOT+resource+'/'+item_id+ACCESS_STRING)
+            res = r.delete(self.API_ROOT + resource + '/' + item_id + self.ACCESS_STRING)
         else:
-            res = r.delete(API_ROOT+resource+'/'+item_id+ACCESS_STRING)
-
+            res = r.delete(self.API_ROOT + resource + self.ACCESS_STRING)
+            
         if res.status_code == 200:
             return res.json()
         else:
-            print(res.status_code, res.json())
-            raise ValueError 
-
+            print(res.status_code)
+            print(res.json())
+            raise ValueError(res.status_code)
